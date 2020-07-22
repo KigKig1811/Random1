695b8c6c597c3139981790f8c3ef581c6eb35e2c
Kig Kig <ngockinh1811@gmai.com>
2020-07-22 19:27:29 +0700

    Random

--- Random/app/src/main/java/com/kig2/random/MainActivity.java
+++ Random/app/src/main/java/com/kig2/random/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
 
@@ -15,15 +17,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 //        int value=tinhTong(5,10);
 //        Log.d("AAA",String.valueOf(value));
-        inThongBao("Test");
+//        inThongBao("Test");
+//        long value=Math.round(1.4);
+       // Log.d("AAA","Round Value" + value);
+         int min = 21;
+         int max = 101;
+         int random = new Random().nextInt((max - min) + 1) +min;
+        Log.d("AAA",random+"");
     }
-    private int tinhTong(int a,int b){
-        int ketqua=a+b;
-        return ketqua;
-    }
-    private void inThongBao(String msg){
-        Toast.makeText(getApplicationContext(),msg,Toast.LENGTH_LONG).show();
-    }
+//    private int tinhTong(int a,int b){
+//        int ketqua=a+b;
+//        return ketqua;
+//    }
+//    private void inThongBao(String msg){
+//        Toast.makeText(getApplicationContext(),msg,Toast.LENGTH_LONG).show();
+//    }
 
 
 
