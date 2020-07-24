015dd3dcae6206de692b7df708d0d8f6469f2f44
Kig Kig <ngockinh1811@gmai.com>
2020-07-24 18:48:47 +0700

    update yêu cầu

--- GameLoTo/app/src/main/java/com/kig2/gameloto/MainActivity.java
+++ GameLoTo/app/src/main/java/com/kig2/gameloto/MainActivity.java
@@ -7,16 +7,17 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
 import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText txtMin,txtMax,txtResult;
-    private Button btnPlay;
+    //global
+     EditText mtxtMin,mtxtMax,mtxtResult;
+     Button mbtnPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //local
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mapping();
@@ -24,20 +25,33 @@ public class MainActivity extends AppCompatActivity {
 
     }
     private void mapping(){
-        txtMin=findViewById(R.id.txtMin);
-        txtMax=findViewById(R.id.txtMax);
-        txtResult=findViewById(R.id.txtResult);
-        btnPlay=findViewById(R.id.btnPlay);
+        mtxtMin=findViewById(R.id.txtMin);
+        mtxtMax=findViewById(R.id.txtMax);
+        mtxtResult=findViewById(R.id.txtResult);
+        mbtnPlay=findViewById(R.id.btnPlay);
     }
      ArrayList arrayList=new ArrayList<Integer>();
     private void playLoTo(){
-        btnPlay.setOnClickListener(new View.OnClickListener() {
+        //Yêu cầu
+        /*
+        1: Nhập dữ liệu :(validate)
+            requied
+            max-min=1
+        2: Xử lí
+            +Hiện thị kêt quả theo chuỗi như sau : a - b - c ...
+            + Các giá trị không được hiện thị 2 lần
+
+
+
+        */
+
+        mbtnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Integer min=Integer.valueOf(txtMin.getText().toString());
-                Integer max=Integer.valueOf(txtMax.getText().toString());
+                Integer min=Integer.valueOf(mtxtMin.getText().toString());
+                Integer max=Integer.valueOf(mtxtMax.getText().toString());
                 int random=new Random().nextInt((max-min)+1)+min;
-                txtResult.setText(""+random);
+                mtxtResult.setText(""+random);
             }
         });
 
