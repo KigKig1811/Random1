14e7075883c3708a5926b3d16d37d38360470f66
Kig Kig <ngockinh1811@gmai.com>
2020-07-29 19:14:12 +0700

    add

--- countdowntimer/app/src/main/java/com/kig2/countdowntimer/MainActivity.java
+++ countdowntimer/app/src/main/java/com/kig2/countdowntimer/MainActivity.java
@@ -3,6 +3,8 @@ package com.kig2.countdowntimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        countDowntimer();
+
+
+    }
+    private void countDowntimer(){
+        CountDownTimer countDownTimer=new CountDownTimer(5000,1000) {
+            @Override
+            public void onTick(long l) {
+                Log.d("AAA",""+l);
+            }
+
+            @Override
+            public void onFinish() {
+                Log.d("AAA","finish");
+            }
+        };
+        countDownTimer.start();
     }
 }
